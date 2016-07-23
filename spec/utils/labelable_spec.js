06bdcc53af1ed970763ba2d7b0e2c8d8bbb50d16
theory friction practice <jackson.brebnor@gmail.com>
2016-07-22 21:14:25 -0400

    augments Labelable.labelizeFunction to accept a host object

--- spec/utils/labelable_spec.js
+++ spec/utils/labelable_spec.js
@@ -1,25 +1,45 @@
 fdescribe('Labelable', () => {
-    var Labelable, decoyFunc;
+    var Labelable, val_func, state0, state1;
     beforeEach(function() {
         Labelable = this.GR.Utils.Labelable;
-        decoyFunc = (arg) => "I am the decoy function";
+        state0 = {
+            label: "state0"
+        };
+        state1 = {
+            label: "state1"
+        };
+        val_func = (state) => state.label;
     });
-    describe('#labelizeFunction(lFunc)', () => {
-        let lRet;
+    describe('#labelizeFunction(val_func)', () => {
+        let lab_func, state_label0, state_label1;
         beforeEach(function() {
-            lRet = Labelable.labelizeFunction(decoyFunc);
+            lab_func = Labelable.labelizeFunction(val_func);
+            state_label0 = lab_func(state0);
+            state_label1 = lab_func(state1);
         });
-        it('returns an object', function() {
-            expect(lRet).toBeObject();
-        });
-        it('returns a label() function', function() {
-            expect(lRet.label).toBeFunction();
-        });
-        describe('returned label', () => {
-            it('executes the original function arg', function() {
-                expect(lRet.label()).toEqual("I am the decoy function");
+        describe('when given a value function', () => {
+            it('returns a second function awaiting a state object', function() {
+                expect(lab_func).toBeFunction();
             });
-
+        });
+        describe('when given a stateObject', () => {
+            it('returns an object', function() {
+                expect(state_label0).toBeObject();
+            });
+            it('returns a label() function', function() {
+                expect(state_label0.label).toBeFunction();
+            });
+            describe('#label', () => {
+                it('executes the original function on the state object', function() {
+                    expect(state_label0.label()).toEqual("state0");
+                });
+            });
+            describe('#label', () => {
+                it('executes the original function on the state object', function() {
+                    expect(state_label1.label()).toEqual("state1");
+                });
+            });
         });
     });
+
 });
