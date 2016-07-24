656a8bb8583f20005e8b96998d7866da48ba8f69
theory friction practice <jackson.brebnor@gmail.com>
2016-07-23 21:42:24 -0400

    caching comparison via curry_equality

--- spec/utils/curry_equality_spec.js
+++ spec/utils/curry_equality_spec.js
@@ -1,5 +1,6 @@
 fdescribe('curry_equality', () => {
-    var curry_equality, labelize, accessor_func, lab_func, state0, state1, s_obj0, s_obj1;
+    let curry_equality, labelize, accessor_func, lab_func;
+    let state0, state1, state2, state3, s_obj0, s_obj1, s_obj2, s_obj3;
     beforeEach(function() {
         curry_equality = this.GR.Utils.curry_equality;
         labelize = this.GR.Utils.labelize;
@@ -10,8 +11,16 @@ fdescribe('curry_equality', () => {
         state1 = {
             val: "state1"
         };
+        state2 = {
+            val: "state0"
+        };
+        state3 = {
+            val: "state3"
+        };
         s_obj0 = Object.assign({}, lab_func(state0));
         s_obj1 = Object.assign({}, lab_func(state1));
+        s_obj2 = Object.assign({}, lab_func(state2));
+        s_obj3 = Object.assign({}, lab_func(state3));
         accessor_func = (obj) => obj.label();
     });
     describe('#curry_equality(accessor_func)', () => {
@@ -48,6 +57,26 @@ fdescribe('curry_equality', () => {
                             });
                         });
 
+                    });
+
+                });
+                describe('when tested against multiple arguments', () => {
+                    let cachedComparitor0, consumedComparitor02, consumedComparitor03;
+                    beforeAll(function() {
+                        cachedComparitor0 = equality_func.compareToHost(s_obj0).isEquivalent;
+                        consumedComparitor02 = cachedComparitor0(s_obj2);
+                        consumedComparitor03 = cachedComparitor0(s_obj3);
+                    });
+                    describe('.isEquivalent ', () => {
+                        it('caches the compareToHost', function() {
+                            expect(cachedComparitor0).toBeFunction();
+                        });
+                        it('can be called against multiple receivers', function() {
+                            console.log(cachedComparitor0);
+                            cachedComparitor0(s_obj2);
+                            console.log(cachedComparitor0);
+
+                        });
                     });
 
                 });
