2964007811f8eefaa851129c573df0ecdc50e8ed
theory friction practice <jackson.brebnor@gmail.com>
2016-07-23 21:01:12 -0400

    are_equal for comparing two objects via one function

---
+++ spec/utils/are_equal_spec.js
@@ -0,0 +1,50 @@
+fdescribe('are_equal', () => {
+    var are_equal, labelize, accessor_func, lab_func, state0, state1, s_obj0, s_obj1;
+    beforeEach(function() {
+        are_equal = this.GR.Utils.are_equal;
+        labelize = this.GR.Utils.labelize;
+        lab_func = labelize((state) => state.val);
+        state0 = {
+            val: "state0"
+        };
+        state1 = {
+            val: "state1"
+        };
+        s_obj0 = Object.assign({}, lab_func(state0));
+        s_obj1 = Object.assign({}, lab_func(state1));
+        accessor_func = (obj) => obj.label();
+        // accessor_func = (state) => state.equality();
+    });
+    describe('#are_equal(accessor_func)', () => {
+        let equality_func, state_equality0, state_equality1;
+        beforeEach(function() {
+            equality_func = are_equal(accessor_func);
+            state_equality0 = equality_func(s_obj0);
+            state_equality1 = equality_func(s_obj1);
+        });
+        describe('when given a value function', () => {
+            it('returns a second function awaiting a host object', function() {
+                expect(equality_func).toBeFunction();
+            });
+            describe('when given a host object', () => {
+                it('returns a third function awaiting a argument object', function() {
+                    expect(state_equality0).toBeFunction();
+                });
+                describe('when given a argument object', () => {
+                    // it('returns a function awaiting a argument object', function() {
+                    //     expect(state_equality0).toBeFunction();
+                    // });
+                    // it('returns a are_equal() function', function() {
+                    //     expect(state_equality0.are_equal).toBeFunction();
+                    // });
+                    // describe('#are_equal', () => {
+                    it('executes the original function on both objects and compares return values', function() {
+                        expect(state_equality0(s_obj0)).toBeTrue();
+                    });
+                });
+
+            });
+        });
+
+    });
+});
