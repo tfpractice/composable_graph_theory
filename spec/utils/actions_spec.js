efbc06079a4f9926d853df1035c40363313c9b9f
theory friction practice <jackson.brebnor@gmail.com>
2016-07-25 11:06:59 -0400

    tests actions func

--- spec/utils/actions_spec.js
+++ spec/utils/actions_spec.js
@@ -1,6 +1,6 @@
 fdescribe('actions', () => {
     var are_equal, labelize, accessor_func, lab_func, state0, state1, s_obj0, s_obj1;
-    let actions, f1, f2, f3, d_func;
+    let actions, f1, sf1, f2, sf2, f3, sf3, d_func;
     beforeAll(function() {
         console.log('\n.........Actions Spec.........');
         actions = this.GR.Utils.actions;
@@ -8,12 +8,34 @@ fdescribe('actions', () => {
     });
     beforeEach(function() {
         f1 = (n) => 2 * n;
+        sf1 = () => (state) => {
+            let ans = f1(state);
+            return {
+                answer: ans,
+                state: ans
+            };
+        };
         f2 = (n) => n + 3;
+        sf2 = () => (state) => {
+            let ans = f1(state);
+            return {
+                answer: ans,
+                state: ans
+            };
+        };
         f3 = (n) => n * 3;
+        sf3 = () => (state) => {
+            let ans = f1(state);
+            return {
+                answer: ans,
+                state: ans
+            };
+        };
         d_func = (n, ns) => ({
             value: n,
             state: ns
         });
+        sd_func = (vals) => vals;
 
         lab_func = labelize((state) => state.val);
         state0 = {
@@ -30,7 +52,7 @@ fdescribe('actions', () => {
     describe('when given an array of actions and a done function', () => {
         let seedFunc;
         beforeEach(function() {
-            seedFunc = actions([f1, f2, f3], d_func);
+            seedFunc = actions([sf1, sf2, sf3], sd_func);
         });
         it('returns a function awaiting seed data', function() {
             expect(seedFunc).toBeFunction();
