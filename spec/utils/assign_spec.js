7c3680c450dc3fa5fc2cdad91ac6a671f52836b0
theory friction practice <jackson.brebnor@gmail.com>
2016-07-25 12:23:28 -0400

    creates assign.js and spec

---
+++ spec/utils/assign_spec.js
@@ -0,0 +1,35 @@
+fdescribe('assign', () => {
+    let assign, actions, liftAnswer, f1, f2, f3, d_func;
+    beforeAll(function() {
+        console.log('\n.........assign Spec.........');
+        assign = this.GR.Utils.assign;
+    });
+    beforeEach(function() {
+        f1 = (n) => ({
+            "f1": () => 2 * n
+        });
+        f2 = (n) => ({
+            "f2": () => n + 3
+        });
+        f3 = (n) => ({
+            "f3": () => n * 3
+        });
+        d_func = (values, state) => ({
+            values,
+            state
+        });
+    });
+    describe('when given a stateHandler function', () => {
+        let func1;
+        beforeEach(function() {
+            func1 = assign(f1);
+        });
+        it('returns a function awaiting a state', function() {
+            expect(func1).toBeFunction();
+        });
+        // it('returns the result of iterated functions on the seed data', function() {
+        //     expect(seedFunc).toBeNumber();
+        // });
+
+    });
+});
