5818f30a7f6d6d87b5d70e38dfd2672781ee24cd
theory friction practice <jackson.brebnor@gmail.com>
2016-07-26 14:04:07 -0400

    baseTypify.js and spec

---
+++ spec/utils/base_typify_spec.js
@@ -0,0 +1,45 @@
+fdescribe('baseTypify', () => {
+    var baseTypify, accessor_func, state0, state1;
+    beforeAll(function() {
+        console.log('\n.........BaseTypify Spec.........');
+        baseTypify = this.GR.Utils.baseTypify;
+    });
+    beforeEach(function() {
+        state0 = {
+            type: "state0"
+        };
+        state1 = {
+            type: "state1"
+        };
+        // accessor_func = (state) => state.type();
+        accessor_func = (state) => state.type;
+    });
+
+    describe('#baseTypifyFunction(accessor_func)', () => {
+        let type_func, state_type0, state_type1;
+        beforeEach(function() {
+            type_func = baseTypify(accessor_func);
+            state_type0 = type_func(state0);
+            state_type1 = type_func(state1);
+        });
+        describe('when given a value function', () => {
+            it('returns a second function awaiting a state object', function() {
+                expect(type_func).toBeFunction();
+            });
+            describe('when given a stateObject', () => {
+                it('returns an object', function() {
+                    expect(state_type0).toBeObject();
+                });
+                it('returns a baseType() function', function() {
+                    expect(state_type0.baseType).toBeFunction();
+                });
+                describe('#type', () => {
+                    it('executes the original function on the state object', function() {
+                        expect(state_type0.baseType()).toEqual("state0");
+                    });
+                });
+            });
+
+        });
+    });
+});
