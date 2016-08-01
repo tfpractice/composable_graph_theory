26871491d93e1e001785a204dfdad03719708a74
theory friction practice <jackson.brebnor@gmail.com>
2016-08-01 13:09:25 -0400

    modifies curry_equality to utilize different accessor functions

--- spec/utils/curry_equality_spec.js
+++ spec/utils/curry_equality_spec.js
@@ -1,11 +1,10 @@
 fdescribe('curry_equality', () => {
-    let curry_equality, labelize, accessor_func, lab_func;
+    let curry_equality, labelize, hostAccess, accessor_func, lab_func;
     let state0, state1, state2, state3, s_obj0, s_obj1, s_obj2, s_obj3;
     beforeAll(function() {
         console.log('\n.........Curry_equality Spec.........');
         curry_equality = this.GR.Utils.curry_equality;
         labelize = this.GR.Utils.labelize;
-
     });
     beforeEach(function() {
         lab_func = labelize((state) => state.val);
@@ -26,6 +25,7 @@ fdescribe('curry_equality', () => {
         s_obj2 = Object.assign({}, lab_func(state2));
         s_obj3 = Object.assign({}, lab_func(state3));
         accessor_func = (obj) => obj.label();
+        hostAccess = (obj) => obj.label();
     });
     describe('#curry_equality(accessor_func)', () => {
         let equality_func, compare_to_state0, compare_to_state1;
@@ -34,7 +34,7 @@ fdescribe('curry_equality', () => {
             compare_to_state0 = equality_func.compareToHost(s_obj0);
             compare_to_state1 = equality_func.compareToHost(s_obj1);
         });
-        describe('when given a value function', () => {
+        describe('when given an accessor function', () => {
             it('returns an object with a compareToHost function ', function() {
                 expect(equality_func).toBeObject();
                 expect(equality_func.compareToHost).toBeFunction();
@@ -60,9 +60,7 @@ fdescribe('curry_equality', () => {
                                 expect(compare_to_state0.isEquivalent(s_obj1)).toBeFalse();
                             });
                         });
-
                     });
-
                 });
                 describe('when tested against multiple arguments', () => {
                     let cachedComparitor0, consumedComparitor02, consumedComparitor03;
@@ -79,13 +77,55 @@ fdescribe('curry_equality', () => {
                             // console.log(cachedComparitor0);
                             cachedComparitor0(s_obj2);
                             // console.log(cachedComparitor0);
-
                         });
                     });
-
                 });
             });
         });
+        describe('when given a host accessor and an argAccessor function', () => {
+            let host0, host1, arg0, arg1, hax, rgax, v2v, h0comp;
+            beforeEach(function() {
+                host0 = {
+                    val: 0
+                };
+                host1 = {
+                    val: 1
+                };
+                arg0 = {
+                    argVal: 0
+                };
+                arg1 = {
+                    argVal: 1
+                };
+                hax = (host) => host.val;
+                rgax = (arg) => arg.argVal;
+                v2v = curry_equality(rgax, hax);
+                h0comp = v2v.compareToHost(host0);
 
+            });
+            describe('curry_equality', () => {
+                it('returns an object containing a compareToHost function', function() {
+                    expect(v2v.compareToHost).toBeTruthy();
+                });
+                describe('when given a host object', () => {
+                    it('returns an isEquivalent function', function() {
+                        expect(h0comp.isEquivalent).toBeTruthy();
+                    });
+                    describe('when given an argObj', () => {
+                        it('comapares the hostAccessor called on the host with the argAccessor called on the arg', function() {
+                            expect(h0comp.isEquivalent(arg0)).toBeTrue();
+                            expect(h0comp.isEquivalent(arg1)).toBeFalse();
+                        });
+                    });
+                });
+            });
+        });
     });
 });
+// 
+// 
+// 
+// 
+// 
+// 
+//
