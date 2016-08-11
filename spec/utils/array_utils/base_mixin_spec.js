6d835b750f8a15e2265a6ab157608728d2e3846b
theory friction practice <jackson.brebnor@gmail.com>
2016-08-11 16:11:03 -0400

    refactors basemixin validator

--- spec/utils/array_utils/base_mixin_spec.js
+++ spec/utils/array_utils/base_mixin_spec.js
@@ -1,36 +1,30 @@
 fdescribe('baseMixin', function() {
-    let arrayOf, baseMixin, nMix, Node, nArray, xArray, myArray, n0, n1, n4, n6, n7, n8, n11, n16, n17, n18, largeArray, fArry;
+    let baseMixin, Node, nMix;
     beforeAll(function() {
-        arrayOf = this.GR.Utils.arrayUtils.arrayOf;
         baseMixin = this.GR.Utils.arrayUtils.baseMixin;
-
         Node = this.GR.Node;
     });
     beforeEach(function() {
         nMix = baseMixin(Node);
         n0 = Node('n0');
-        n1 = Node('n1');
-        n4 = Node('n4');
-        myArray = nMix([n0, n1, n4]);
     });
-
-    describe('when given an array', () => {
-        it('returns a object ', function() {
-            expect((myArray)).toBeObject();
+    describe('when given a type', () => {
+        it('returns an object ', function() {
+            expect((nMix)).toBeObject();
         });
         describe('.baseType', () => {
             it('returns the name of the class of objects allowed into the array ', function() {
-                expect(myArray.baseType()).toEqual("Node");
+                expect(nMix.baseType()).toEqual("Node");
             });
         });
         describe('type', () => {
             it('retunrs the baseType + "Array"', function() {
-                expect(myArray.type()).toBe("NodeArray");
+                expect(nMix.type()).toBe("NodeArray");
             });
         });
         describe('isValid(elem)', () => {
             it('validates an element', function() {
-                // expect(myArray.isValid(n0)).toBeTrue();
+                expect(nMix.isValid(n0)).toBeTrue();
             });
         });
     });
