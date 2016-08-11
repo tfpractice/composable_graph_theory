88f07b9dc398cff9e041d84fad79e00b3efd3af9
theory friction practice <jackson.brebnor@gmail.com>
2016-08-11 13:49:13 -0400

    composeMixin spec

---
+++ spec/utils/array_utils/compose_mixin_spec.js
@@ -0,0 +1,37 @@
+fdescribe('composeMixin', function() {
+    let composeMixin, m0, m1, mxarg, myComp;
+    beforeAll(function() {
+        console.log('\n.........composeMixin Spec.........');
+        funcValidator = this.GR.Utils.funcValidator;
+        checker = this.GR.Utils.checker;
+        composeMixin = this.GR.Utils.arrayUtils.composeMixin;
+    });
+    beforeEach(function() {
+        m0 = (Base) => ({
+            method0: () => "i am method0"
+        });
+        m1 = (Base) => ({
+            method1: () => "i am method1"
+        })
+        mxarg = {};
+        myComp = composeMixin(m0, m1);
+    });
+    describe('when given a series of mixins', () => {
+        it('returns a function ', function() {
+            expect((myComp)).toBeFunction();
+        });
+        describe('when given arg object', () => {
+            beforeEach(function() {
+                myComp = myComp(mxarg)
+            });
+            it('retunrs an object containing mixin methods', function() {
+                expect(myComp).toBeObject();
+                expect(myComp.method0).toBeTruthy();
+                expect(myComp.method1).toBeTruthy();
+            });
+            describe('when given valid input', () => {
+                it('retunrs a empty array', function() {});
+            });
+        });
+    });
+});
