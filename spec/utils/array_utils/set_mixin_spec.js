b711e5d08940fdce3460c15da5effa9aed2d6c81
theory friction practice <jackson.brebnor@gmail.com>
2016-08-12 20:41:05 -0400

    typeMixin spec work independently

--- spec/utils/array_utils/set_mixin_spec.js
+++ spec/utils/array_utils/set_mixin_spec.js
@@ -1,4 +1,4 @@
-fdescribe('setMixin', function() {
+describe('setMixin', function() {
     let setMixin, typeMixin, composeMixin, composedInstance, basicInstance;
     let arrayOf;
     let n0, n1, n2, n3, n4;
--- spec/utils/array_utils/type_mixin_spec.js
+++ spec/utils/array_utils/type_mixin_spec.js
@@ -1,9 +1,12 @@
 fdescribe('typeMixin', function() {
     let typeMixin, nMix, tMix0, tMix1, composeMixin, mixtype, mycomp, altMix, comboMix, a0, aMod0, a1, instanceMethod
+    let setMixin;
     beforeAll(function() {
         arrayOf = this.GR.Utils.arrayUtils.arrayOf;
         typeMixin = this.GR.Utils.arrayUtils.typeMixin;
+        setMixin = this.GR.Utils.arrayUtils.setMixin;
         composeMixin = this.GR.Utils.arrayUtils.composeMixin;
+
         Node = this.GR.Node;
         nMix = typeMixin(Node);
     });
@@ -12,14 +15,14 @@ fdescribe('typeMixin', function() {
             myLen: () => sArr.length,
             mySlice: () => iFunc(sArr.slice(0))
         });
-        mycomp = (iFunc) => (sArr = []) => composeMixin(typeMixin(iFunc), altMix(iFunc))(sArr);
+        mycomp = (iFunc) => (sArr = []) => composeMixin(typeMixin(iFunc), setMixin(iFunc), altMix(iFunc))(sArr);
         instanceMethod = (sArr = []) => Object.assign(Array.from(sArr), mycomp(instanceMethod)(sArr));
         mixtype = instanceMethod;
         tMix0 = typeMixin(instanceMethod);
         n0 = Node('n0');
         n1 = Node('n1');
         n4 = Node('n4');
-        a0 = tMix0([2, 3, 4]);
+        a0 = tMix0([n0, n1, n4]);
         aMod0 = a0.concat(9);
         a1 = mixtype(a0);
         myArray = nMix([n0, n1, n4]);
@@ -43,6 +46,46 @@ fdescribe('typeMixin', function() {
             describe('concat', () => {
                 it('calls the instance method on array with the concatenated values', function() {
                     expect(aMod0).toBeArray();
+                });
+            });
+            describe('slice', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.slice(0)).toBeArray();
+                });
+            });
+            describe('splice', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.splice(0)).toBeArray();
+                });
+            });
+            describe('reassign', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.reassign()).toBeArray();
+                });
+            });
+            describe('isEmpty', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.isEmpty()).toBeFalse();
+                });
+            });
+            describe('filter', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.filter(e => e.label() == "n1")).toBeArray();
+                });
+            });
+            describe('clear', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.clear()).toBeArray();
+                });
+            });
+            describe('copy', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.copy()).toBeArray();
+                });
+            });
+            describe('removeElement', function() {
+                it('calls the instanceFunc on the return val from slice', function() {
+                    expect(a0.removeElement(n1)).toBeObject();
                 });
             });
         });
