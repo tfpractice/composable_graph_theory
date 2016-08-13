6c716e39661ecc02985b8b0bd9c66f134f2b4fcc
theory friction practice <jackson.brebnor@gmail.com>
2016-08-12 21:32:10 -0400

    fixed bs array isntance issues

--- spec/utils/array_utils/set_mixin_spec.js
+++ spec/utils/array_utils/set_mixin_spec.js
@@ -1,30 +1,43 @@
-describe('setMixin', function() {
+fdescribe('setMixin', function() {
     let setMixin, typeMixin, composeMixin, composedInstance, basicInstance;
-    let arrayOf;
+    let arrayOf, nonenum, constructor;
+    let a;
     let n0, n1, n2, n3, n4;
-    let nMix, sMix0, sMix1, mixtype, mycomp, altMix, comboMix, a0, aMod0, a1;
+    let nMix, sMix0, sMix1, mixtype, mycomp, composed, compKeys, altMix, comboMix, a0, aMod0, a1;
     beforeAll(function() {
         console.log('\n.........setMixin Spec.........');
         Node = this.GR.Node;
+        nonenum = this.GR.Utils.nonEnum;
+
         arrayOf = this.GR.Utils.arrayUtils.arrayOf.arrayOf;
         typeMixin = this.GR.Utils.arrayUtils.typeMixin;
         setMixin = this.GR.Utils.arrayUtils.setMixin;
         composeMixin = this.GR.Utils.arrayUtils.composeMixin;
-        console.log(arrayOf);
+        // console.log(arrayOf);
 
-        basicInstance = (sArr = []) => Object.assign(Array.from(sArr), Array.from(sArr), setMixin(basicInstance)(sArr));
+        // composed = (iFunc) => (sArr = []) => composeMixin(typeMixin(iFunc), setMixin(iFunc), altMix(iFunc))(sArr);
+        compKeys = (iFunc) => (sArr = []) => Object.keys(setMixin(iFunc)(sArr));
+        instanceMethod = (sArr = []) => nonenum(constructor(sArr))(...compKeys(constructor)(sArr))
+        // constructor = (sArr = []) => Object.assign(Array.from(sArr), mycomp(instanceMethod)(sArr));
+        constructor = (sArr = []) => Object.assign(Array.from(sArr), setMixin(constructor)(sArr));
+        // basicInstance = (sArr = []) => Object.assign(Array.from(sArr), setMixin(basicInstance)(sArr));
     });
     beforeEach(function() {
         // basicInstance = arrayOf(Node).instance;
 
-        sMix0 = setMixin(basicInstance);
+        sMix0 = setMixin(instanceMethod);
         n0 = Node('n0');
         n1 = Node('n1');
         n2 = Node('n2');
         n3 = Node('n3');
         n4 = Node('n4');
-        a0 = sMix0([n0, n1, n4]);
-        a1 = sMix0([n0, n1, n2, n3, n4]);
+        a = [n0, n1, n4];
+        a0 = constructor(a);
+        // a1 = sMix0([n0, n1, n2, n3, n4]);
+        a1 = [n0, n1, n2, n3, n4];
+        // console.log("a0", a0);
+        // console.log("a0 props", Object.getOwnPropertyNames(a0));
+        // console.log("compKeys", compKeys(constructor)(a0));
     });
     it('is a function', function() {
         expect(setMixin).toBeFunction();
@@ -36,7 +49,7 @@ describe('setMixin', function() {
         });
         describe('when given an array', () => {
             it('returns an object', function() {
-                expect(a0).toBeObject();
+                expect(a0).toBeArray();
             });
             describe('contains', () => {
                 it('returns a boolean regarding the presence of an element in the array', function() {
--- spec/utils/array_utils/type_mixin_spec.js
+++ spec/utils/array_utils/type_mixin_spec.js
@@ -1,13 +1,17 @@
 fdescribe('typeMixin', function() {
-    let typeMixin, nMix, tMix0, tMix1, composeMixin, mixtype, mycomp, altMix, comboMix, a0, aMod0, a1, instanceMethod
-    let setMixin;
+    let typeMixin, nMix, tMix0, tMix1, composeMixin, mixtype, mycomp, altMix, comboMix, a, a0, a2, aMod0, a1, instanceMethod
+    let setMixin, nonenum, constructor;
     beforeAll(function() {
+        console.log('\n.........typeMixin Spec.........');
+
         arrayOf = this.GR.Utils.arrayUtils.arrayOf;
         typeMixin = this.GR.Utils.arrayUtils.typeMixin;
         setMixin = this.GR.Utils.arrayUtils.setMixin;
         composeMixin = this.GR.Utils.arrayUtils.composeMixin;
 
         Node = this.GR.Node;
+        nonenum = this.GR.Utils.nonEnum;
+
         nMix = typeMixin(Node);
     });
     beforeEach(function() {
@@ -16,15 +20,37 @@ fdescribe('typeMixin', function() {
             mySlice: () => iFunc(sArr.slice(0))
         });
         mycomp = (iFunc) => (sArr = []) => composeMixin(typeMixin(iFunc), setMixin(iFunc), altMix(iFunc))(sArr);
-        instanceMethod = (sArr = []) => Object.assign(Array.from(sArr), mycomp(instanceMethod)(sArr));
-        mixtype = instanceMethod;
+        let myKeys = (iFunc) => (sArr = []) => Object.keys(mycomp(iFunc)(sArr));
+        instanceMethod = (sArr = []) => nonenum(constructor(sArr))(...myKeys(constructor)(sArr))
+        constructor = (sArr = []) => Object.assign(Array.from(sArr), mycomp(instanceMethod)(sArr));
+        // mixtype = instanceMethod;
         tMix0 = typeMixin(instanceMethod);
         n0 = Node('n0');
         n1 = Node('n1');
         n4 = Node('n4');
-        a0 = tMix0([n0, n1, n4]);
+
+        a1 = tMix0(a0);
+        a = [n0, n1, n4];
+        // a0 = tMix0([n0, n1, n4]);
+
+        a0 = instanceMethod(a);
         aMod0 = a0.concat(9);
-        a1 = mixtype(a0);
+
+
+        a2 = instanceMethod(a);
+        let compa0 = mycomp(instanceMethod)(a0);
+        let a3 = Object.assign(Array.from(a0), compa0);
+        let a4 = instanceMethod(a0);
+        // console.log(mycomp(instanceMethod)(a0))
+        // console.log("a2", a2);
+
+        // console.log("compa0", compa0);
+        // console.log("a3 length", a3.length);
+        // console.log("a3 props", Object.getOwnPropertyNames(a3));
+        // console.log("a4 length", a4.length);
+
+        // console.log("a4 props", Object.getOwnPropertyNames(a4));
+
         myArray = nMix([n0, n1, n4]);
     });
     it('is a function', function() {
@@ -41,7 +67,8 @@ fdescribe('typeMixin', function() {
         });
         describe('when given an array', () => {
             it('returns an object', function() {
-                expect(a0).toBeObject();
+                // expect(a0).toBeObject();
+                expect(a0).toBeArray();
             });
             describe('concat', () => {
                 it('calls the instance method on array with the concatenated values', function() {
@@ -85,7 +112,15 @@ fdescribe('typeMixin', function() {
             });
             describe('removeElement', function() {
                 it('calls the instanceFunc on the return val from slice', function() {
-                    expect(a0.removeElement(n1)).toBeObject();
+                    expect(a0.removeElement(n1)).toBeArray();
+                });
+            });
+        });
+        describe('when mixed with setMixin', () => {
+            describe('contains', () => {
+                it('returns a boolean regarding the presence of an element in the array', function() {
+                    // console.log(a0);
+                    expect(a0.contains(n4)).toBeTrue();
                 });
             });
         });
