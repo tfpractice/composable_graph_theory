7511bd11e56ec5ffec75690ae60934a2b357260d
theory friction practice <jackson.brebnor@gmail.com>
2016-08-15 09:27:00 -0400

    typemixing now curried

--- spec/utils/array_utils/type_mixin_spec.js
+++ spec/utils/array_utils/type_mixin_spec.js
@@ -3,15 +3,12 @@ fdescribe('typeMixin', function() {
     let setMixin, nonenum, constructor;
     beforeAll(function() {
         console.log('\n.........typeMixin Spec.........');
-
         arrayOf = this.GR.Utils.arrayUtils.arrayOf;
         typeMixin = this.GR.Utils.arrayUtils.typeMixin;
         setMixin = this.GR.Utils.arrayUtils.setMixin;
         composeMixin = this.GR.Utils.arrayUtils.composeMixin;
-
         Node = this.GR.Node;
         nonenum = this.GR.Utils.nonEnum;
-
         nMix = typeMixin(Node);
     });
     beforeEach(function() {
@@ -23,34 +20,18 @@ fdescribe('typeMixin', function() {
         let myKeys = (iFunc) => (sArr = []) => Object.keys(mycomp(iFunc)(sArr));
         instanceMethod = (sArr = []) => nonenum(constructor(sArr))(...myKeys(constructor)(sArr))
         constructor = (sArr = []) => Object.assign(Array.from(sArr), mycomp(instanceMethod)(sArr));
-        // mixtype = instanceMethod;
         tMix0 = typeMixin(instanceMethod);
         n0 = Node('n0');
         n1 = Node('n1');
         n4 = Node('n4');
-
         a1 = tMix0(a0);
         a = [n0, n1, n4];
-        // a0 = tMix0([n0, n1, n4]);
-
         a0 = instanceMethod(a);
         aMod0 = a0.concat(9);
-
-
         a2 = instanceMethod(a);
         let compa0 = mycomp(instanceMethod)(a0);
         let a3 = Object.assign(Array.from(a0), compa0);
         let a4 = instanceMethod(a0);
-        // console.log(mycomp(instanceMethod)(a0))
-        // console.log("a2", a2);
-
-        // console.log("compa0", compa0);
-        // console.log("a3 length", a3.length);
-        // console.log("a3 props", Object.getOwnPropertyNames(a3));
-        // console.log("a4 length", a4.length);
-
-        // console.log("a4 props", Object.getOwnPropertyNames(a4));
-
         myArray = nMix([n0, n1, n4]);
     });
     it('is a function', function() {
@@ -67,7 +48,6 @@ fdescribe('typeMixin', function() {
         });
         describe('when given an array', () => {
             it('returns an object', function() {
-                // expect(a0).toBeObject();
                 expect(a0).toBeArray();
             });
             describe('concat', () => {
@@ -119,7 +99,6 @@ fdescribe('typeMixin', function() {
         describe('when mixed with setMixin', () => {
             describe('contains', () => {
                 it('returns a boolean regarding the presence of an element in the array', function() {
-                    // console.log(a0);
                     expect(a0.contains(n4)).toBeTrue();
                 });
             });
