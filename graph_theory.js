e976d4df1739058953c053b60d49e246bc2cd983
theory friction practice <jackson.brebnor@gmail.com>
2016-08-16 12:31:31 -0400

    Edge function and eqaulity methods

--- graph_theory.js
+++ graph_theory.js
@@ -1,3 +1,4 @@
 exports.Node = require('./lib/node');
 exports.NodeArray = require('./lib/node_array');
+exports.Edge = require('./lib/edge');
 exports.Utils = require('./lib/graph_utils');
---
+++ spec/edge_spec.js
@@ -0,0 +1,53 @@
+fdescribe('Edge', function() {
+    let Node, Edge, NodeArray;
+    let n00, n01, n10, n11, n20, n21;
+    let e0, e1, e2;
+    beforeAll(function() {
+        console.log('\n.........Edge Spec.........');
+        Node = this.GR.Node;
+        NodeArray = this.GR.NodeArray;
+        Edge = this.GR.Edge;
+        // console.log(Node);
+    });
+    beforeEach(function() {
+        [n00, n01, n10, n11, n20, n21] = ["n00", "n01", "n10", "n11", "n20", "n21"].map(Node);
+        console.log(n00);
+        e0 = Edge(n00, n01);
+        e1 = Edge(n10, n11);
+        e2 = Edge(n20, n21);
+    });
+    describe('nodes', () => {
+        it('returns a NodeArray instance', function() {
+            expect(e0.nodes()).toBeArray();
+            expect(e0.nodes().contains).toBeTruthy();
+        });
+    });
+    describe('weight()', () => {
+
+    });
+    describe('toString()', () => {
+        it('returns the label prefixed by "[ Edge" ', function() {
+            expect(e0.toString()).toContain('[ Edge ');
+        });
+    });
+    describe('label', () => {
+        it('returns the concatenated labels of each of the nodes', function() {
+            expect(e0.label()).toBe('n00::n01');
+        });
+    });
+    describe('hasSameName', () => {
+        it('returns a boolean based on label equality', function() {
+            expect(e0.hasSameName(e1)).toBeFalse();
+        });
+    });
+    describe('hasSameNodes', () => {
+        it('returns a boolean based on equality of the nodes', function() {
+            expect(e0.hasSameNodes(e1)).toBeFalse();
+        });
+    });
+    describe('containsNode', () => {
+        it('returns aboolean based on presence of node in nodes', function() {
+            expect(e0.containsNode(n00)).toBeTrue();
+        });
+    });
+});
