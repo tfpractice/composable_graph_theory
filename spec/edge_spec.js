b6363690553009e6cb9feb5913dd9f5210c87019
theory friction practice <jackson.brebnor@gmail.com>
2016-08-16 12:39:55 -0400

    edge.weight

--- spec/edge_spec.js
+++ spec/edge_spec.js
@@ -11,7 +11,7 @@ fdescribe('Edge', function() {
     });
     beforeEach(function() {
         [n00, n01, n10, n11, n20, n21] = ["n00", "n01", "n10", "n11", "n20", "n21"].map(Node);
-        console.log(n00);
+        // console.log(n00);
         e0 = Edge(n00, n01);
         e1 = Edge(n10, n11);
         e2 = Edge(n20, n21);
