4c452cdb2ca7880cb19d728f5f9fbf19d361d5e9
theory friction practice <jackson.brebnor@gmail.com>
2016-08-17 16:05:40 -0400

    tests edgeArray Basics

--- graph_theory.js
+++ graph_theory.js
@@ -1,4 +1,5 @@
 exports.Node = require('./lib/node');
 exports.NodeArray = require('./lib/node_array');
 exports.Edge = require('./lib/edge');
+exports.EdgeArray = require('./lib/edge_array');
 exports.Utils = require('./lib/graph_utils');
--- spec/edge_array_spec.js
+++ spec/edge_array_spec.js
@@ -1,18 +1,39 @@
-describe('EdgeArray', function() {
-    let Node, NodeArray, EdgeArray;
-    let myNode, myArray, myAltArray, n1, n2, n3, n4;
+fdescribe('EdgeArray', function() {
+
+    let Node, NodeArray, Edge, EdgeArray;
+    let myNode, myArray, myAltArray;
+    let n00, n01, n10, n11, n20, n21, n30, n31;
+    let e0, e1, e2, e3;
     beforeAll(function() {
-        console.log('\n.........NodeArray Spec.........');
-        // subArray = this.GR.Utils.subArray;
+        console.log('\n.........EdgeArray Spec.........');
         Node = this.GR.Node;
         NodeArray = this.GR.NodeArray;
+        Edge = this.GR.Edge;
         EdgeArray = this.GR.EdgeArray;
     });
     beforeEach(function() {
-        [n00, n01, n10, n11, n20, n21] = ["n00", "n01", "n10", "n11", "n20", "n21"].map(Node);
+        [n00, n01, n10, n11, n20, n21, n30, n31] = ["n00", "n01", "n10", "n11", "n20", "n21", "n30", "n31"].map(Node);
         // console.log(n00);
         e0 = Edge(n00, n01);
         e1 = Edge(n10, n11);
         e2 = Edge(n20, n21, 10);
+        e3 = Edge(n30, n31);
+        console.log(EdgeArray.instance);
+        myArray = EdgeArray.instance([e0, e1]);
+        // myArray.push(n3);
+        // myArray.push(n4);
+        // myAltArray = EdgeArray.instance([n1]);
+        // myAltArray.push(n2);
+        // myAltArray.push(n3);
+        // console.log(EdgeArray);
+    });
+    it('is an object', function() {
+        expect(EdgeArray).toBeObject();
+    });
+    describe('instance', () => {
+        it('returns an instance of EdgeArray', function() {
+            console.log(myArray.map(eArr => eArr.nodes()));
+            expect(myArray).toBeArray();
+        });
     });
 });
