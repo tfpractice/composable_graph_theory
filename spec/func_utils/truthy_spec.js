1c4c8034640d487c8b6afc5ec1603f3568341311
theory friction practice <jackson.brebnor@gmail.com>
2016-08-17 19:54:01 -0400

    truthy

---
+++ spec/func_utils/truthy_spec.js
@@ -0,0 +1,29 @@
+fdescribe('truthy', function() {
+    let truthy, tVal;
+    beforeAll(function() {
+        console.log('\n.........truthy Spec.........');
+        truthy = this.GR.FuncUtils.truthy;
+        tVal = 3;
+
+    });
+    describe('when given null', () => {
+        it('returns false', function() {
+            expect(truthy(null)).toBeFalse();
+        });
+    });
+    describe('when given undefined', () => {
+        it('returns false', function() {
+            expect(truthy(undefined)).toBeFalse();
+        });
+    });
+    describe('when given false', () => {
+        it('returns true', function() {
+            expect(truthy(false)).toBeFalse();
+        });
+    });
+    describe('when given a truthy value', () => {
+        it('returns true', function() {
+            expect(truthy(tVal)).toBeTrue();
+        });
+    });
+});
