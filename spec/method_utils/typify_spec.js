eaa89637819962b7dce393d758896e06b50f69c5
theory friction practice <jackson.brebnor@gmail.com>
2016-08-18 10:34:48 -0400

    meth/typifyspec

---
+++ spec/method_utils/typify_spec.js
@@ -0,0 +1,45 @@
+describe('typify', () => {
+    var typify, accessor_func, state0, state1;
+    beforeAll(function() {
+        console.log('\n.........Typify Spec.........');
+        typify = this.GR.MethodUtils.typify;
+    });
+    beforeEach(function() {
+        state0 = {
+            type: () => "state0"
+        };
+        state1 = {
+            type: () => "state1"
+        };
+        // accessor_func = (state) => state.type();
+        accessor_func = (state) => "theType";
+    });
+    describe('#typifyFunction(accessor_func)', () => {
+        let type_func, state_type0, state_type1;
+        beforeEach(function() {
+            type_func = typify(accessor_func);
+            state_type0 = type_func(state0);
+            state_type1 = type_func(state1);
+        });
+        describe('when given a value function', () => {
+            it('returns a second function awaiting a state object', function() {
+                expect(type_func).toBeFunction();
+            });
+            describe('when given a stateObject', () => {
+                it('returns an object', function() {
+                    expect(state_type0).toBeObject();
+                });
+                it('returns a type() function', function() {
+                    expect(state_type0.type).toBeFunction();
+                });
+                describe('#type', () => {
+                    it('executes the original function on the state object', function() {
+                        expect(state_type0.type()).toEqual("theType");
+                    });
+                });
+
+            });
+        });
+
+    });
+});
