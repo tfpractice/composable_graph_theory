6647b7fa67e018d4a93874cc8efd012dcb47c509
theory friction practice <jackson.brebnor@gmail.com>
2016-08-19 19:33:24 -0400

    adds contains methods directly onto context object and makes it non enumerable

--- spec/array_utils/containify_spec.js
+++ spec/array_utils/containify_spec.js
@@ -1,12 +1,17 @@
 describe('containify', function() {
     let containify, cFunc, cState, myContext, xContext, myQuery;
+    let modify, modContext, hidContext, hideMethod;
     beforeAll(function() {
         console.log('\n.........containify Spec.........');
         containify = this.GR.ArrayUtils.containify;
+        modify = containify.modify;
+        hideMethod = containify.hideMethod;
         cFunc = (query) => (el) => ((el + query) % 2) === 0;
         cState = containify(cFunc);
         myContext = [2, 6, 8, 10];
         xElements = [9, 3, 7, 5];
+        modContext = modify(cFunc)(Array.from(myContext));
+        hidContext = hideMethod(cFunc)(Array.from(myContext));
 
     });
     describe('when given a curryable function', () => {
@@ -25,5 +30,37 @@ describe('containify', function() {
                 });
             });
         });
+    });
+    describe('modify', () => {
+        describe('when given a curryable function', () => {
+            it('returns a function', function() {
+                expect(modify(cFunc)).toBeFunction();
+            });
+
+            describe('when given a  collection', () => {
+                it('returns an Array', function() {
+                    expect(modContext).toBeArray();
+                });
+                describe('Object.keys', () => {
+                    it('contains "contains"', function() {
+                        expect(Object.keys(modContext)).toContain('contains');
+
+                    });
+                });
+                describe('hideMethod', () => {
+                    it('removes "contains" from Object.keys', function() {
+                        expect(hidContext).toBeArray();
+                        expect(Object.keys(hidContext)).not.toContain('contains');
+                    });
+
+                });
+                describe('contains', () => {
+                    it('applies the curried function to the query and calls it on each element', function() {
+                        expect(modContext.contains(2)).toBeTrue();
+                    });
+                });
+            });
+        });
+
     });
 });
