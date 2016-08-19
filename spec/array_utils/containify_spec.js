428f98ddf78e2ca0130fe984ddb85215b6f6dc2a
theory friction practice <jackson.brebnor@gmail.com>
2016-08-19 18:25:46 -0400

    arrayutils/containify

--- spec/array_utils/containify_spec.js
+++ spec/array_utils/containify_spec.js
@@ -1,30 +1,29 @@
-// describe('containify', function() {
-//     let containify, cFunc, cState, myNodes;
-//     beforeAll(function() {
-//         console.log('\n.........containify Spec.........');
-//         containify = this.GR.MethodUtils.containify;
-//         cFunc = (state) => state.nodes;
-//         cState = {
-//             nodes: 10
-//         };
-//         myNodes = containify(cFunc)(cState);
+fdescribe('containify', function() {
+    let containify, cFunc, cState, myContext, xContext, myQuery;
+    beforeAll(function() {
+        console.log('\n.........containify Spec.........');
+        containify = this.GR.ArrayUtils.containify;
+        cFunc = (query) => (el) => ((el + query) % 2) === 0;
+        cState = containify(cFunc);
+        myContext = [2, 6, 8, 10];
+        xElements = [9, 3, 7, 5];
 
-//     });
-//     describe('when given a nodes function', () => {
-//         it('returns a function', function() {
-//             expect(containify(cFunc)).toBeFunction();
-//         });
+    });
+    describe('when given a curryable function', () => {
+        it('returns a function', function() {
+            expect(cState).toBeFunction();
+        });
 
-//         describe('when given a state obj', () => {
-//             it('returns an object', function() {
-//                 expect(myNodes).toBeObject();
-//             });
+        describe('when given a set of collection', () => {
+            it('returns an object', function() {
+                expect(cState(myContext)).toBeObject();
+            });
 
-//             describe('nodes', () => {
-//                 it('returns the wfuc called on the state', function() {
-//                     expect(myNodes.nodes()).toBe(10);
-//                 });
-//             });
-//         });
-//     });
-// });
+            describe('contains', () => {
+                it('applies the curried function to the query and calls it on each element', function() {
+                    expect(cState(myContext).contains(2)).toBeTrue();
+                });
+            });
+        });
+    });
+});
