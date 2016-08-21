e856aad9fc9d61d3e864c78dde73340639c6a0c7
theory friction practice <jackson.brebnor@gmail.com>
2016-08-21 19:18:04 -0400

    setmixin adjustements

--- spec/array_utils/set_mixin_spec.js
+++ spec/array_utils/set_mixin_spec.js
@@ -3,7 +3,8 @@ describe('setMixin', function() {
     let arrayOf, nonenum, constructor;
     let a, ca1;
     let n0, n1, n2, n3, n4;
-    let nMix, sMix0, sMix1, mixtype, mycomp, composed, compKeys, altMix, comboMix, a0, aMod0, a1;
+    let nMix, sMix0, sMix1, mixtype, mycomp, composed, compKeys, altMix,
+        comboMix, a0, aMod0, a1;
     beforeAll(function() {
         console.log('\n.........setMixin Spec.........');
         Node = this.GR.Node;
@@ -15,9 +16,12 @@ describe('setMixin', function() {
         setMixin = this.GR.ArrayUtils.setMixin;
         composeMixin = this.GR.ArrayUtils.composeMixin;
         // composeMixin = this.GR.Utils.arrayUtils.composeMixin;
-        compKeys = (iFunc) => (sArr = []) => Object.keys(setMixin(iFunc)(sArr));
-        instanceMethod = (sArr = []) => nonenum(constructor(sArr))(...compKeys(constructor)(sArr))
-        constructor = (sArr = []) => Object.assign(Array.from(sArr), setMixin(instanceMethod)(sArr));
+        compKeys = (iFunc) => (sArr = []) => Object.keys(setMixin(iFunc)
+            (sArr));
+        instanceMethod = (sArr = []) => nonenum(constructor(sArr))(...compKeys(
+            constructor)(sArr))
+        constructor = (sArr = []) => Object.assign(Array.from(sArr),
+            setMixin(instanceMethod)(sArr));
     });
     beforeEach(function() {
         sMix0 = setMixin(instanceMethod);
@@ -44,65 +48,83 @@ describe('setMixin', function() {
                 expect(sMix0(a)).toBeObject();
             });
             describe('contains', () => {
-                it('returns a boolean regarding the presence of an element in the array', function() {
-                    expect(mxa.contains(n4)).toBeTrue();
-                });
+                it(
+                    'returns a boolean regarding the presence of an element in the array',
+                    function() {
+                        expect(mxa.contains(n4)).toBeTrue();
+                    });
             });
             describe('hasSameSize(altArray)', () => {
-                it('returns a boolean regarding the equality of the array sizes', function() {
-                    expect(mxa.hasSameSize([1, 2, 3])).toBeTrue();
-                });
+                it(
+                    'returns a boolean regarding the equality of the array sizes',
+                    function() {
+                        expect(mxa.hasSameSize([1, 2, 3])).toBeTrue();
+                    });
             });
             describe('isSubset(altArray)', function() {
-                it('returns a boolean regarding the presnece of this arrays values in another array', function() {
-                    expect(mxa.isSubset(a1)).toBeTrue();
-                });
+                it(
+                    'returns a boolean regarding the presnece of this arrays values in another array',
+                    function() {
+                        expect(mxa.isSubset(a1)).toBeTrue();
+                    });
             });
             describe('isEquivalent(altArray', () => {
-                it('returns a boolean based on eqaulity of the arrays', function() {
-                    expect(a0.isEquivalent(a0)).toBeTrue();
-                    expect(a0.isEquivalent(ca1)).toBeFalse();
-                });
+                it(
+                    'returns a boolean based on eqaulity of the arrays',
+                    function() {
+                        expect(a0.isEquivalent(a0)).toBeTrue();
+                        expect(a0.isEquivalent(ca1)).toBeFalse();
+                    });
             });
             describe('findEquivalentElement ', () => {
-                it('returns an equivalent element in the array', function() {
-                    expect(a0.findEquivalentElement(n4)).toBe(n4);
-                });
+                it('returns an equivalent element in the array',
+                    function() {
+                        expect(a0.findEquivalentElement(n4)).toBe(
+                            n4);
+                    });
             });
             describe('intersects ', () => {
-                it('returns a boolean based on the presence of shared elements', function() {
-                    expect(a0.intersects(a1)).toBeTrue();
-                });
+                it(
+                    'returns a boolean based on the presence of shared elements',
+                    function() {
+                        expect(a0.intersects(a1)).toBeTrue();
+                    });
             });
             describe('intersection ', () => {
-                it('returns an array of the shared elements', function() {
-                    let shared = a0.intersection(ca1)
-                    expect(shared).toBeArray();
-                    expect(shared).toContain(...a0);
-                });
+                it('returns an array of the shared elements',
+                    function() {
+                        let shared = a0.intersection(ca1)
+                        expect(shared).toBeArray();
+                        expect(shared).toContain(...a0);
+                    });
             });
             describe('hasDistinctElements ', () => {
-                it('returns a booolean based on the absence of elemnets in the current array in the altArray', function() {
-                    // console.log(a0.length);
-                    // console.log(ca1.length);
-                    expect(ca1.hasDistinctElements(a0)).toBeTrue();
-                    expect(a0.hasDistinctElements(ca1)).toBeFalse();
-                    expect(a0.hasDistinctElements(a0)).toBeFalse();
-                });
+                it(
+                    'returns a booolean based on the absence of elemnets in the current array in the altArray',
+                    function() {
+                        // console.log(a0.length);
+                        // console.log(ca1.length);
+                        expect(ca1.hasDistinctElements(a0)).toBeTrue();
+                        expect(a0.hasDistinctElements(ca1)).toBeFalse();
+                        expect(a0.hasDistinctElements(a0)).toBeFalse();
+                    });
             });
             describe('difference ', () => {
-                it('returns an array of elemnets in the current array absent in the altArray', function() {
-                    expect(ca1.difference(a0)).toBeArray();
-                    expect(ca1.difference(a0)).toContain(n2);
-                });
+                it(
+                    'returns an array of elemnets in the current array absent in the altArray',
+                    function() {
+                        expect(ca1.difference(a0)).toBeArray();
+                        expect(ca1.difference(a0)).toContain(n2);
+                    });
             });
             describe('union ', () => {
-                it('returns all elements in either array', function() {
-                    let un = a0.union(ca1);
-                    // console.log(un);
-                    expect(un).toBeArray();
-                    expect(un).toContain(n2);
-                });
+                it('returns all elements in either array',
+                    function() {
+                        let un = a0.union(ca1);
+                        // console.log(un);
+                        expect(un).toBeArray();
+                        expect(un).toContain(n2);
+                    });
             });
             describe('unionize ', () => {
                 it('merges the two arrays', function() {
@@ -114,20 +136,26 @@ describe('setMixin', function() {
                 });
             });
             describe('push ', () => {
-                describe('when passed an element alrady present', () => {
-                    it('does not change the length of the array', function() {
-                        let alen = a0.length;
-                        a0 = a0.push(n1);
-                        expect(a0.length).toEqual(alen);
+                describe(
+                    'when passed an element alrady present', () => {
+                        it(
+                            'does not change the length of the array',
+                            function() {
+                                let alen = a0.length;
+                                a0 = a0.push(n1);
+                                expect(a0.length).toEqual(alen);
+                            });
                     });
-                });
-                describe('when passed an element not alrady present', () => {
-                    it('increments thelength of the array', function() {
-                        let alen = a0.length;
-                        a0 = a0.push(n2);
-                        expect(a0.length).toEqual(alen + 1);
+                describe(
+                    'when passed an element not alrady present', () => {
+                        it('increments thelength of the array',
+                            function() {
+                                let alen = a0.length;
+                                a0 = a0.push(n2);
+                                expect(a0.length).toEqual(alen +
+                                    1);
+                            });
                     });
-                });
             });
 
         });
@@ -143,10 +171,20 @@ describe('setMixin', function() {
     });
     describe('operators', () => {
         describe('isPresent(query)', () => {
-            it('returns a boolean regarding the persence of an element in an array', function() {
-                expect(setMixin.isPresent(n3)(a0)).toBeFalse();
-                expect(setMixin.isPresent(n3)(ca1)).toBeTrue();
-            });
+            it(
+                'returns a boolean regarding the persence of an element in an array',
+                function() {
+                    expect(setMixin.isPresent(n3)(a0)).toBeFalse();
+                    expect(setMixin.isPresent(n3)(ca1)).toBeTrue();
+                });
+        });
+        describe('contains(query)', () => {
+            it(
+                'returns a boolean regarding the persence of an element in an array',
+                function() {
+                    expect(setMixin.contains(n3)(a0)).toBeFalse();
+                    expect(setMixin.contains(n3)(ca1)).toBeTrue();
+                });
         });
     });
 
