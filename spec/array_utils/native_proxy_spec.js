71f7dcddeee9e9326e2861dbb05de5a66b29bbac
theory friction practice <jackson.brebnor@gmail.com>
2016-08-22 14:17:34 -0400

    native splice

--- spec/array_utils/native_proxy_spec.js
+++ spec/array_utils/native_proxy_spec.js
@@ -1,42 +1,49 @@
 describe('nativeProxy', function() {
-    let nativeProxy, pushProxy, pop;
+    let nativeProxy, pushProxy, spliceProxy;
     let queryA, contextA, xContext;
     beforeAll(function() {
         console.log('\n.........nativeProxy Spec.........');
         nativeProxy = this.GR.ArrayUtils.nativeProxy;
         pushProxy = nativeProxy.pushProxy;
+        spliceProxy = nativeProxy.spliceProxy;
+        queryA = [1, 2, 3];
+        contextA = [3, 4, 5];
+        xContext = [9, 7];
+    });
+    beforeEach(function() {
         queryA = [1, 2, 3];
         contextA = [3, 4, 5];
         xContext = [9, 7];
     });
     describe('pushProxy', () => {
         describe('when given a context', () => {
-            it('returns a function', function() {
+            it('returns a function', () => {
                 expect(pushProxy(contextA)).toBeFunction();
             });
         });
         describe('when given arguments', () => {
             it(
-                'calls array.prototype.pushProxy on the context\
-                 with the args',
-                function() {
+                'calls array.prototype.pushProxy on the context with the args', () => {
                     let oldLength = contextA.length;
                     pushProxy(contextA)(8);
                     expect(contextA.length).toBe(oldLength + 1);
-                    expect(
-                        pushProxy(contextA)(8)).toBeArray();
+                    expect((pushProxy(contextA)(8))).toBeArray();
                 });
         });
     });
-    // describe('when given a query array', () => {
-    //     it('returns a function', function() {
-    //         expect(nativeProxy(queryA)).toBeFunction();
-    //     });
-    //     describe('when given a context Array', () => {
-    //         it('compares the lengths', function() {
-    //             expect(nativeProxy(queryA)(contextA)).toBeTrue();
-    //             expect(nativeProxy(queryA)(xContext)).toBeFalse();
-    //         });
-    //     });
-    // });
+    describe('spliceProxy', () => {
+        describe('when given a context', () => {
+            it('returns a function', () => {
+                expect(spliceProxy(contextA)).toBeFunction();
+            });
+        });
+        describe('when given arguments', () => {
+            it(
+                'calls array.prototype.splice on the context with the args', () => {
+                    let splArr = spliceProxy(contextA)(1);
+                    expect(splArr).toBeArray();
+                    expect(splArr).not.toContain(3);
+                });
+        });
+    });
 });
