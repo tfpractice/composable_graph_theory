b503a852fd17a0ec9d2d7815210f72744770e017
theory friction practice <jackson.brebnor@gmail.com>
2016-08-23 12:13:36 -0400

    lark

---
+++ spec/func_utils/combinators/lark_spec.js
@@ -0,0 +1,31 @@
+fdescribe('lark', function() {
+    let combinators, thunkify, f0, f1, f2;
+    let lark;
+    let x, y;
+    beforeAll(function() {
+        console.log('\n.........lark Spec.........');
+        thunkify = this.GR.FuncUtils.thunkify;
+        combinators = this.GR.FuncUtils.combinators;
+        lark = combinators.lark;
+        x = 30;
+        y = 20;
+        f0 = thunkify(x);
+        f1 = thunkify(y);
+        f2 = (y) => 2 * y;
+    });
+    describe('lark', () => {
+        it('is a function', function() {
+            expect(lark).toBeFunction();
+        });
+        describe('when given function x', () => {
+            it('returns  a function ', function() {
+                expect(lark(f2)).toBeFunction();
+            });
+            describe('when given a second function', () => {
+                it('calls x on y called upon itself', function() {
+                    expect(lark(f2)(f1)).toBe(40);
+                });
+            });
+        });
+    });
+});
