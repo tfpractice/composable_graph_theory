91b5ec016e128c10608783df10f6ad8cf88bb51e
theory friction practice <jackson.brebnor@gmail.com>
2016-08-26 13:46:48 -0400

    curries factory mixin

--- spec/array_utils/factory_mixin_spec.js
+++ spec/array_utils/factory_mixin_spec.js
@@ -33,32 +33,46 @@ describe('factoryMixin', function() {
     });
     describe('factoryMixin', () => {
         describe('when given a overriding function', () => {
-            it('returns a function', function() {
+            it('returns a function with properties', function() {
                 expect(rFactory).toBeFunction();
                 expect(dFactory).toBeFunction();
             });
+            it('has the array spawning functions as properties', () => {
+                expect(rFactory.concat).toBeFunction();
+                expect(dFactory.concat).toBeFunction();
+                expect(rFactory.slice).toBeFunction();
+                expect(dFactory.slice).toBeFunction();
+                expect(rFactory.splice).toBeFunction();
+                expect(dFactory.splice).toBeFunction();
+                expect(rFactory.filter).toBeFunction();
+                expect(dFactory.filter).toBeFunction();
+            });
         });
-        describe('when given a context', () => {
-            it('returns an object', () => {
-                // console.log(rFactory(contextA).concat.toString());
-                expect(rFactory(contextA)).toBeObject();
-                expect(dFactory(contextA)).toBeObject();
-            });
-            it('has all of the native_override functions', () => {
-                expect(rFactory(contextA).concat).toBeFunction();
-                expect(dFactory(contextA).concat).toBeFunction();
-                expect(rFactory(contextA).slice).toBeFunction();
-                expect(dFactory(contextA).slice).toBeFunction();
-                expect(rFactory(contextA).splice).toBeFunction();
-                expect(dFactory(contextA).splice).toBeFunction();
-                expect(rFactory(contextA).filter).toBeFunction();
-                expect(dFactory(contextA).filter).toBeFunction();
-            });
-            describe('when producing a new array', () => {
-                it('calls the factory function on the return',
-                    function() {
 
-                    });
+    });
+    describe('when given a context', () => {
+        it('returns an object', () => {
+            // console.log(rFactory(contextA).concat.toString());
+            expect(rFactory(contextA)).toBeObject();
+            expect(dFactory(contextA)).toBeObject();
+        });
+        it('has all of the native_override functions', () => {
+            expect(rFactory(contextA).concat).toBeFunction();
+            expect(dFactory(contextA).concat).toBeFunction();
+            expect(rFactory(contextA).slice).toBeFunction();
+            expect(dFactory(contextA).slice).toBeFunction();
+            expect(rFactory(contextA).splice).toBeFunction();
+            expect(dFactory(contextA).splice).toBeFunction();
+            expect(rFactory(contextA).filter).toBeFunction();
+            expect(dFactory(contextA).filter).toBeFunction();
+        });
+        describe('when spawning a new array', () => {
+            it('calls the factory function on the new array', () => {
+                let dubCatA = dFactory(contextA).concat(
+                    contextA
+                );
+                expect(dubCatA.length).toEqual(contextA.length *
+                    4);
             });
         });
     });
