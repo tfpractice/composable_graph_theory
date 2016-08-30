3f96a4e60e98010aa6617f8f2d4da9e42f480678
theory friction practice <jackson.brebnor@gmail.com>
2016-08-30 13:44:40 -0400

    getWeight/sameWeight

--- spec/method_utils/weighable_spec.js
+++ spec/method_utils/weighable_spec.js
@@ -8,23 +8,30 @@ describe('weighable', function() {
             weight: 10
         };
         myWeight = weighable(wFunc)(wState);
-
     });
     describe('when given a weight function', () => {
-        it('returns a function', function() {
+        it('returns a function', () => {
             expect(weighable(wFunc)).toBeFunction();
         });
-
         describe('when given a state obj', () => {
-            it('returns an object', function() {
+            it('returns an object', () => {
                 expect(myWeight).toBeObject();
             });
-
             describe('weight', () => {
-                it('returns the wfuc called on the state', function() {
+                it('returns the wfuc called on the state', () => {
                     expect(myWeight.weight()).toBe(10);
                 });
             });
+        });
+    });
+    describe('getWeight', () => {
+        it('returns the weight value', () => {
+            expect(weighable.getWeight(myWeight)).toBe(10);
+        });
+    });
+    describe('sameWeight', () => {
+        it('returns the weight value', () => {
+            expect(weighable.sameWeight(myWeight)(myWeight)).toBeTrue();
         });
     });
 });
