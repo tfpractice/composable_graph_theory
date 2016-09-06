6443fe73169229a96503d944868985e0a6121bf4
theory friction practice <jackson.brebnor@gmail.com>
2016-09-06 11:34:45 -0400

    datable module

--- spec/method_utils/datafy_spec.js
+++ spec/method_utils/datafy_spec.js
@@ -1,55 +1,4 @@
-// fdescribe('datafy', () => {
-//     var datafy, accessor_func, state0, state1;
-//     beforeAll(function() {
-//         console.log('\n.........Datafy Spec.........');
-//         datafy = this.GR.MethodUtils.datafy;
-//     });
-//     beforeEach(function() {
-//         state0 = {
-//             data: "state0"
-//         };
-//         state1 = {
-//             data: "state1"
-//         };
-//         // accessor_func = (state) => state.data();
-//         accessor_func = (state) => state.data;
-//     });
-//     describe('#datafyFunction(accessor_func)', () => {
-//         let data_func, state_data0, state_data1;
-//         beforeEach(function() {
-//             data_func = datafy(accessor_func);
-//             state_data0 = data_func(state0);
-//             state_data1 = data_func(state1);
-//         });
-//         describe('when given a value function', () => {
-//             it('returns a second function awaiting a state object', function() {
-//                 expect(data_func).toBeFunction();
-//             });
-//             describe('when given a stateObject', () => {
-//                 it('returns an object', function() {
-//                     expect(state_data0).toBeObject();
-//                 });
-//                 it('returns a data() function', function() {
-//                     expect(state_data0.data).toBeFunction();
-//                 });
-//                 describe('#data', () => {
-//                     it(
-//                         'executes the original function on the state object',
-//                         function() {
-//                             expect(state_data0.data()).toEqual(
-//                                 "state0");
-//                             expect(state_data1.data()).toEqual(
-//                                 "state1");
-//                         });
-//                 });
-
-//             });
-//         });
-
-//     });
-// });
-
-fdescribe('datafy', () => {
+describe('datafy', () => {
     var datafy, myFunc, state0, state1;
     let myData, sData0, sData1;
     let getData, sameData;
@@ -102,7 +51,7 @@ fdescribe('datafy', () => {
         describe('sameData', () => {
             it('it compares the data() with getData() on the argument', () => {
                 expect(sData0.sameData(sData0)).toBeTrue();
-                expect(sData0.sameData(sData1)).not.toBeFalse();
+                expect(sData0.sameData(sData1)).toBeFalse();
             });
         });
     });
