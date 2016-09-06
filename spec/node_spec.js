ef1cf4dbb65ae859a6c8a5ba79c612b4b5476173
theory friction practice <jackson.brebnor@gmail.com>
2016-09-06 11:55:03 -0400

    prep to cheange node

--- spec/node_spec.js
+++ spec/node_spec.js
@@ -1,8 +1,7 @@
-describe('Node', function() {
+fdescribe('Node', function() {
     let Node;
     let myNode, ns0, ns1, ns2, ns3;
     let myState, n2;
-
     beforeAll(function() {
         console.log('\n.........Node Spec.........');
         Node = this.GR.Node;
@@ -13,68 +12,66 @@ describe('Node', function() {
         myNode = Node(2, 0);
     });
     describe('.toString()', () => {
-        it('returns "Node"', function() {
+        it('returns "Node"', () => {
             expect(Node.toString()).toBe('Node');
         });
     });
     describe('.Node(lable, data) ', () => {
 
-        it('returns a new Node object', function() {
+        it('returns a new Node object', () => {
             expect(myNode).toBeObject();
         });
-        it('matches the #type of each instance', function() {
+        it('matches the #type of each instance', () => {
             expect(n2.type()).toBe(Node.toString());
         });
         describe('#label()', () => {
-            it('has a #label method', function() {
+            it('has a #label method', () => {
                 expect(myNode.label).toBeFunction();
             });
-            it('returns the first argument of the params list',
-                function() {
-                    expect(myNode.label()).toBe(2);
-                });
+            it('returns the first argument of the params list', () => {
+                expect(myNode.label()).toBe(2);
+            });
         });
         describe('#type()', () => {
-            it('has a #type method', function() {
+            it('has a #type method', () => {
                 expect(myNode.type).toBeFunction();
             });
-            it('returns `Node`', function() {
+            it('returns `Node`', () => {
                 expect(myNode.type()).toEqual('Node');
             });
         });
         describe('#data()', () => {
-            it('has a #data method', function() {
+            it('has a #data method', () => {
                 expect(myNode.data).toBeFunction();
             });
-            it('returns the second argument', function() {
+            it('returns the second argument', () => {
                 expect(myNode.data()).toBe(0);
             });
         });
         describe('#isEquivalent', () => {
-            it('returns true if the two objects share label',
-                function() {
-                    expect(myNode.isEquivalent(n2)).toBeTrue();
-                });
+            it('returns true if the two objects share label', () => {
+                expect(myNode.isEquivalent(n2)).toBeTrue();
+            });
         });
     });
     describe('operators', () => {
         describe('getLabel(node)', () => {
-            it('returns the nodes label', function() {
+            it('returns the nodes label', () => {
                 expect(Node.getLabel(n2)).toBe(n2.label());
             });
         });
         describe('getData(node)', () => {
-            it('returns the nodes label', function() {
+            it('returns the nodes label', () => {
                 expect(Node.getData(n2)).toBe(n2.data());
             });
         });
         describe('getType(node)', () => {
-            it('returns the nodes label', function() {
+            it('returns the nodes label', () => {
                 expect(Node.getType(n2)).toBe(n2.type());
             });
         });
         describe('isEquivalent(argNode)(srcNode)', () => {
-            it('returns a boolean based on node equality', function() {
+            it('returns a boolean based on node equality', () => {
                 expect(Node.isEquivalent(n2)(myNode)).toBeTrue();
             });
         });
