351a489f31f0d22eb7da3dfb3558cc912202700c
theory friction practice <jackson.brebnor@gmail.com>
2016-09-08 13:26:05 -0400

    factories and subtypes

--- spec/method_utils/factory_spec.js
+++ spec/method_utils/factory_spec.js
@@ -1,27 +1,32 @@
-fdescribe('Factory', function() {
-    let Factory, fState;
-    let myFactory, n0, n00, n1, n2, n3;
+fdescribe('factory', function() {
+    let factory, fState;
+    let myfactory, n0, n00, n1, n2, n3, subN0;
+    let myExtension;
     let myState, state0, state1, state2, state3;
     let typify, tFunc, nType;
     let datafy, dFunc, nData;
     let equatable, eqFunc, nEq;
     let labelize, lFunc, nLabel;
+    let t_lFunc, t_nLabel;
     beforeAll(function() {
-        console.log('\n.........Factory Spec.........');
-        Factory = this.GR.MethodUtils.factory;
+        console.log('\n.........factory Spec.........');
+        factory = this.GR.MethodUtils.factory;
         equalize = this.GR.MethodUtils.equalize;
         datafy = this.GR.MethodUtils.datafy;
         labelize = this.GR.MethodUtils.labelize;
         typify = this.GR.MethodUtils.typify;
-        tFunc = (s) => "Factory",
+        tFunc = (s) => "factory",
         dFunc = (state) => state.data;
         lFunc = (state) => state.label;
+        t_lFunc = (state) => "factoryTest" + state.label,
         eqFunc = state => arg => (labelize(lFunc)(state).sameLabel(arg));
         nType = typify(tFunc);
         nData = datafy(dFunc);
         nEq = equalize(eqFunc);
         nLabel = labelize(lFunc);
-        myFactory = Factory(nType, nData, nLabel, nEq)
+        t_nLabel = labelize(t_lFunc);
+        myfactory = factory(nType, nData, nLabel, nEq)
+        myExtension = myfactory.subType(t_nLabel);
     });
     beforeEach(function() {
         state0 = {
@@ -30,15 +35,15 @@ fdescribe('Factory', function() {
                 position: 0
             }
         };
-        n0 = myFactory(state0);
+        n0 = myfactory(state0);
         state1 = {
             label: "node1",
             data: {
                 position: 1
             }
         };
-        n1 = myFactory(state1);
-        n00 = myFactory({
+        n1 = myfactory(state1);
+        n00 = myfactory({
             state: state0.state,
             label: state1.label
         });
@@ -48,29 +53,30 @@ fdescribe('Factory', function() {
                 position: 2
             }
         };
-        n2 = myFactory(state2);
+        n2 = myfactory(state2);
         state3 = {
             label: "node3",
             data: {
                 position: 3
             }
         };
-        n3 = myFactory(state3);
+        n3 = myfactory(state3);
+        subN0 = myExtension(state0);
     });
     it('is a function', function() {
-        expect(Factory).toBeFunction();
+        expect(factory).toBeFunction();
     });
     describe('.toString()', () => {
-        it('returns "Factory"', () => {});
+        it('returns "factory"', () => {});
     });
     describe('when given a set of mixins', () => {
         it('returns a function with operators', function() {
-            expect(Factory(nType, nData, nLabel, nEq)).toBeFunction();
+            expect(factory(nType, nData, nLabel, nEq)).toBeFunction();
         });
     });
     describe('when given a state object', () => {
-        describe('.Factory(label, data) ', () => {
-            it('returns a new Factory object', () => {
+        describe('.factory(label, data) ', () => {
+            it('returns a new factory object', () => {
                 expect(n2).toBeObject();
             });
             describe('#label()', () => {
@@ -80,9 +86,9 @@ fdescribe('Factory', function() {
                 });
             });
             describe('#type()', () => {
-                it('returns `Factory`', () => {
+                it('returns `factory`', () => {
                     expect(n0.type).toBeFunction();
-                    expect(n0.type()).toEqual('Factory');
+                    expect(n0.type()).toEqual('factory');
                 });
             });
             describe('#data()', () => {
@@ -100,23 +106,62 @@ fdescribe('Factory', function() {
         describe('operators', () => {
             describe('getLabel(node)', () => {
                 it('returns the nodes label', () => {
-                    expect(myFactory.getLabel(n2)).toBe(n2.label());
+                    expect(myfactory.getLabel(n2)).toBe(n2.label());
                 });
             });
             describe('getData(node)', () => {
                 it('returns the nodes label', () => {
-                    expect(myFactory.getData(n2)).toBe(n2.data());
+                    expect(myfactory.getData(n2)).toBe(n2.data());
                 });
             });
             describe('getType(node)', () => {
                 it('returns the nodes label', () => {
-                    expect(myFactory.getType(n2)).toBe(n2.type());
+                    expect(myfactory.getType(n2)).toBe(n2.type());
                 });
             });
-            describe('isEquivalent(argFactory)(srcFactory)', () => {
+            describe('isEquivalent(argfactory)(srcfactory)', () => {
                 it('returns a boolean based on node equality', () => {
-                    expect(myFactory.isEquivalent(n0)(n0)).toBeTrue();
-                    expect(myFactory.isEquivalent(n0)(n2)).toBeFalse();
+                    expect(myfactory.isEquivalent(n0)(n0)).toBeTrue();
+                    expect(myfactory.isEquivalent(n0)(n2)).toBeFalse();
+                });
+            });
+        });
+    });
+    describe('mixins', () => {
+        it('is an array', () => {
+            expect(myfactory.mixins).toBeArray();
+        });
+        it('is nonenumerable', () => {
+            expect(Object.keys(myfactory)).not.toContain('mixins');
+        });
+    });
+    describe('subType', () => {
+        it('is a function', () => {
+            expect(myExtension).toBeFunction();
+        });
+        it('is nonenumerable', () => {
+            expect(Object.keys(myfactory)).not.toContain('subType');
+        });
+        describe('when given more mixins', () => {
+            it('applies new mixins to factory', () => {
+                expect(subN0.label).toBeFunction();
+                expect(subN0.label()).toBe('factoryTestnode0');
+            });
+        });
+    });
+    describe('subType', () => {
+        describe('when given a factory', () => {
+            it('returns an augmented  factory', () => {
+                expect(factory.subType(myfactory)).toBeFunction();
+            });
+            describe('when given more mixins', () => {
+                it('applies new mixins to factory', () => {
+                    let mySubFact = factory.subType(myfactory)(
+                        t_nLabel);
+                    let subTypeN0 = mySubFact(state0);
+                    expect(factory.subType(myfactory)(t_nLabel)).toBeFunction();
+                    expect(subTypeN0.label).toBeFunction();
+                    expect(subTypeN0.label()).toBe('factoryTestnode0');
                 });
             });
         });
