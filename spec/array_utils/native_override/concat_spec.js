cdb85ece8d7e3223f89e6bc5007e9e09d90945ca
theory friction practice <jackson.brebnor@gmail.com>
2016-09-16 12:51:00 -0400

    adds jscs

--- spec/array_utils/native_override/concat_spec.js
+++ spec/array_utils/native_override/concat_spec.js
@@ -1,57 +1,58 @@
-describe('concatOverride', function() {
-    let nativeOverride, concatOverride;
-    let reverser, doubler;
-    let getFirst, getLast, getLength, copy;
-    let rConcat, dConcat, rdcat;
-    let queryA, contextA, xContext;
-    beforeAll(function() {
-        console.log('\n.........concatOverride Spec.........');
-        nativeOverride = this.GR.ArrayUtils.nativeOverride;
-        concatOverride = nativeOverride.concatOverride;
-        reverser = (coll) => Array.from(coll).reverse();
-        doubler = (coll) => coll.concat(coll);
-        getFirst = (coll) => coll.slice(0, 1).pop();
-        getLast = (coll) => coll.slice(-1).pop();
-        getLength = (coll) => coll.length;
-        rConcat = concatOverride(reverser);
-        dConcat = concatOverride(doubler);
-    });
-    beforeEach(function() {
-        queryA = [1, 2, 3];
-        contextA = [3, 4, 5];
-        xContext = [9, 7];
-    });
-    describe('instanceFunctions', () => {
-        describe('reverser', () => {
-            it('takes an array and reverses its elements', () => {
-                let aRev = reverser(queryA);
-                let aDub = doubler(queryA);
-                expect(getFirst(aRev)).toBe(getLast(queryA));
-                expect(aDub.length).toEqual(queryA.length * 2);
-            });
-        });
-    });
-    describe('concatOverride', () => {
-        describe('when given a overriding function', () => {
-            it('returns a function', function() {
-                expect(rConcat).toBeFunction();
-                expect(dConcat).toBeFunction();
-            });
+describe('concatOverride', function () {
+  let nativeOverride, concatOverride;
+  let reverser, doubler;
+  let getFirst, getLast, getLength, copy;
+  let rConcat, dConcat, rdcat;
+  let queryA, contextA, xContext;
+  beforeAll(function () {
+    console.log('\n.........concatOverride Spec.........');
+    nativeOverride = this.GR.ArrayUtils.nativeOverride;
+    concatOverride = nativeOverride.concatOverride;
+    reverser = (coll) => Array.from(coll).reverse();
+    doubler = (coll) => coll.concat(coll);
+    getFirst = (coll) => coll.slice(0, 1).pop();
+    getLast = (coll) => coll.slice(-1).pop();
+    getLength = (coll) => coll.length;
+    rConcat = concatOverride(reverser);
+    dConcat = concatOverride(doubler);
+  });
 
-        });
-        describe('when given a context', () => {
-            it('returns a function', () => {
-                expect(rConcat(contextA)).toBeFunction();
-                expect(rConcat(contextA)).toBeFunction();
-            });
-        });
-        describe('when given arguments', () => {
-            it('calls iFunc on [[array]]concat(context, args)', () => {
-                let revQ = rConcat(queryA)(contextA)
-                let qContext = queryA.concat(contextA);
-                expect(revQ).toBeArray();
-                expect(getFirst(revQ)).toBe(getLast(qContext));
-            });
-        });
+  beforeEach(function () {
+    queryA = [1, 2, 3];
+    contextA = [3, 4, 5];
+    xContext = [9, 7];
+  });
+
+  describe('instanceFunctions', () => {
+    describe('reverser', () => {
+      it('takes an array and reverses its elements', () => {
+        let aRev = reverser(queryA);
+        let aDub = doubler(queryA);
+        expect(getFirst(aRev)).toBe(getLast(queryA));
+        expect(aDub.length).toEqual(queryA.length * 2);
+      });
     });
+  });
+  describe('concatOverride', () => {
+    describe('when given a overriding function', () => {
+      it('returns a function', function () {
+        expect(rConcat).toBeFunction();
+        expect(dConcat).toBeFunction();
+      });
+    });
+    describe('when given a context', () => {
+      it('returns a function', () => {
+        expect(rConcat(contextA)).toBeFunction();
+        expect(rConcat(contextA)).toBeFunction();
+      });
+    });
+    describe('when given arguments', () => {
+      it('calls iFunc on [[array]]concat(context, args)', () => {
+        let revQ = rConcat(queryA)(contextA);
+        let qContext = queryA.concat(contextA);
+        expect(revQ).toBeArray();
+        expect(getFirst(revQ)).toBe(getLast(qContext));
+      });
+    });
+  });
 });
