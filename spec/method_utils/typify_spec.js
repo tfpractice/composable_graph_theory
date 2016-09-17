e4facf0949f4cfc6c6baa8cf6f0d4d4b4cd9482f
theory friction practice <jackson.brebnor@gmail.com>
2016-09-16 23:48:09 -0400

    typify with state focus

--- spec/method_utils/typify_spec.js
+++ spec/method_utils/typify_spec.js
@@ -2,7 +2,7 @@ fdescribe('typify', () => {
     var typify, myFunc, state0, state1;
     let myType, sType0, sType1;
     let getType, sameType;
-    let tStruct;
+    let tStruct, stateOps;
     beforeAll(function () {
         console.log('\n.........typify Spec.........');
         typify = this.GR.MethodUtils.typify;
@@ -11,6 +11,8 @@ fdescribe('typify', () => {
         myType = typify(myFunc);
         getType = myType.getType;
         sameType = myType.sameType;
+        stateOps = typify.stateOps(myFunc);
+
     });
 
     beforeEach(function () {
@@ -23,7 +25,7 @@ fdescribe('typify', () => {
     it('is a function', () => {
         expect(typify).toBeFunction();
     });
-    describe('when given an accessor function lFunc', () => {
+    describe('when given an accessor function tFunc', () => {
         it('returns a second function awaiting a state object', () => {
             expect(myType).toBeFunction();
             expect(myType.getType).toBeFunction();
@@ -43,6 +45,7 @@ fdescribe('typify', () => {
             });
         });
     });
+
     describe('methods', () => {
         describe('type', () => {
             it('retrieves the type attribute ', () => {
@@ -56,6 +59,38 @@ fdescribe('typify', () => {
                     expect(sType0.sameType(sType0)).toBeTrue();
                     expect(sType0.sameType(sType1)).toBeFalse();
                 });
+        });
+    });
+    describe('stateOps ', () => {
+        describe('when when passed a typeFunction', () => {
+            it('returns a function with props', () => {
+                expect(stateOps).toBeFunction();
+                expect(stateOps.getType).toBeFunction();
+                expect(stateOps.sameType).toBeFunction();
+            });
+        });
+        describe('when when passed a stateObject', () => {
+            it('returns an object with props', () => {
+                let s0 = stateOps(state0);
+                expect(s0).toBeObject();
+                expect(s0.type).toBeFunction();
+                expect(s0.sameType).toBeFunction();
+            });
+            describe('type', () => {
+                it('retrieves the type attribute ', () => {
+                    expect(stateOps(state0).type())
+                        .toBe('state0');
+                });
+            });
+            describe('sameType', () => {
+                it(
+                    'it compares the tFunc on both state objects',
+                    () => {
+                        let s0 = stateOps(state0);
+                        expect(s0.sameType(state0)).toBeTrue();
+                        expect(s0.sameType(state1)).toBeFalse();
+                    });
+            });
         });
     });
 });
