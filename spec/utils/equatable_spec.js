f20ab50dc3ad7c149da50788936551980e83eb09
theory friction practice <jackson.brebnor@gmail.com>
2016-09-17 18:58:02 -0400

    equatable

--- spec/utils/equatable_spec.js
+++ spec/utils/equatable_spec.js
@@ -5,21 +5,22 @@ describe('equalize', () => {
     beforeAll(function() {
         console.log('\n.........equalize Spec.........');
         equalize = this.GR.MethodUtils.equalize;
-        myFunc = (state) => (arg) => state.weight === arg.weight;
+        stateOps = equalize.stateOps(myFunc);
+        eqStruct = (weight = '') => ({ weight });
+        myFunc = ({ weight: w0 = 6 }) => ({ weight: w1 = 6 }) =>
+            w0 === w1;
         myEquivalence = equalize(myFunc);
         isEquivalent = myEquivalence.isEquivalent;
         notEquivalent = myEquivalence.notEquivalent;
     });
+
     beforeEach(function() {
-        state0 = {
-            weight: "state0"
-        };
-        state1 = {
-            weight: "state1"
-        };
+        state0 = eqStruct(0);
+        state1 = eqStruct(1);
         sEquivalence0 = myEquivalence(state0);
         sEquivalence1 = myEquivalence(state1);
     });
+
     it('is a function', () => {
         expect(equalize).toBeFunction();
     });
@@ -32,28 +33,36 @@ describe('equalize', () => {
     describe('operators', () => {
         describe('isEquivalent', () => {
             it('compares eqFunc on both objects', () => {
-                expect(isEquivalent(sEquivalence0)(state0)).toBeTrue();
-                expect(isEquivalent(sEquivalence0)(state1)).toBeFalse();
+                expect(isEquivalent(sEquivalence0)(state0))
+                    .toBeTrue();
+                expect(isEquivalent(sEquivalence0)(state1))
+                    .toBeFalse();
             });
         });
         describe('notEquivalent', () => {
             it('compares eqFunc on both objects', () => {
-                expect(notEquivalent(sEquivalence0)(state1)).toBeTrue();
-                expect(notEquivalent(sEquivalence0)(state0)).toBeFalse();
+                expect(notEquivalent(sEquivalence0)(
+                    state1)).toBeTrue();
+                expect(notEquivalent(sEquivalence0)(
+                    state0)).toBeFalse();
             });
         });
     });
     describe('methods', () => {
         describe('isEquivalent', () => {
             it('it compares the eqFunc() on the argument', () => {
-                expect(sEquivalence0.isEquivalent(state0)).toBeTrue();
-                expect(sEquivalence1.isEquivalent(state0)).toBeFalse();
+                expect(sEquivalence0.isEquivalent(state0))
+                    .toBeTrue();
+                expect(sEquivalence1.isEquivalent(state0))
+                    .toBeFalse();
             });
         });
         describe('notEquivalent', () => {
             it('it compares the eqFunc() on the argument', () => {
-                expect(sEquivalence0.notEquivalent(state1)).toBeTrue();
-                expect(sEquivalence1.notEquivalent(state1)).toBeFalse();
+                expect(sEquivalence0.notEquivalent(state1))
+                    .toBeTrue();
+                expect(sEquivalence1.notEquivalent(state1))
+                    .toBeFalse();
             });
         });
     });
