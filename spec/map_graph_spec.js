eaabbb552b3da0a2b0fd05d9abe5711facb85543
theory friction practice <jackson.brebnor@gmail.com>
2016-09-19 14:23:08 -0400

    mgraph class

--- spec/map_graph_spec.js
+++ spec/map_graph_spec.js
@@ -21,19 +21,75 @@ fdescribe('mGraph', function() {
 		stateX = nStruct('nodeX', { position: 0 });
 		nodeX = Node(stateX);
 		nodeXX = Node(stateX);
-
-		// console.log(n0.isEquivalent(n000));
 		n0 = Node(state0);
-
 		n1 = Node(state1);
-
 		n2 = Node(state2);
-
 		n3 = Node(state3);
 		myGraph = mGraph(n0, n1, n2, n3);
 	});
 
-	it('is an object', function() {
+	it('is a function', function() {
+		expect(mGraph).toBeFunction();
+	});
+
+	describe('when given a set of elements', () => {
+		it('retuns an object', function() {
+			expect(mGraph(n0, n1, n2, n3)).toBeObject();
+			expect(myGraph.nodes).toBeTruthy();
+			expect(myGraph.edges).toBeTruthy();
+			expect(myGraph.neighbors).toBeTruthy();
+			expect(myGraph.contains).toBeTruthy();
+			expect(myGraph.addEdge).toBeTruthy();
+			expect(myGraph.removeEdge).toBeTruthy();
+			expect(myGraph.isAdjacent).toBeTruthy();
+		});
+
+		describe('nodes', () => {
+			it('is a set', () => {
+				expect(myGraph.nodes instanceof Set).toBeTrue();
+			});
+		});
+		describe('edges', () => {
+			it('is a Map', () => {
+				expect(myGraph.edges instanceof Map).toBeTrue();
+			});
+		});
+		describe('neighbors(node)', () => {
+			it('returns the associated nodes edges entry', () => {
+				expect(myGraph.neighbors(n0) instanceof Map).toBeTrue();
+			});
+		});
+		describe('contains', () => {
+			it('it checks the node[Set] for the presence of an object', () => {
+				expect(myGraph.contains(n1)).toBeTrue();
+				expect(myGraph.contains('hello')).toBeFalse();
+			});
+		});
+		describe('addEdge(n0)(n1,weight)', () => {
+			it('updates each nodes edge entry', () => {
+				myGraph.addEdge(n0)(n1, 2);
+				expect(myGraph.neighbors(n0).has(n1)).toBeTrue();
+				expect(myGraph.neighbors(n0).get(n1)).toBe(2);
+			});
+		});
+		describe('removeEdge', () => {
+			it('removes an entry from the edgs map', () => {
+				myGraph.addEdge(n0)(n1, 2);
+				myGraph.removeEdge(n0)(n1);
+				expect(myGraph.neighbors(n0).has(n1)).toBeFalse();
+				expect(myGraph.neighbors(n0).get(n1)).toBeUndefined();
+			});
+		});
+		describe('isAdjacent', () => {
+			it('is checks for a node in the neighbor map', () => {
+				myGraph.addEdge(n0)(n1, 2);
+				expect(myGraph.neighbors(n1).has(n0)).toBeTrue();
+				expect(myGraph.isAdjacent(n1)(n0)).toBeTrue();
+			});
+		});
+	});
+
+	it('is an object', () => {
 		expect(myGraph).toBeObject();
 	});
 });
